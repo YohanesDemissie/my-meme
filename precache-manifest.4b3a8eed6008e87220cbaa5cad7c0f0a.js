5f44ec1eb14027830c282debb11329773640594a
Yohanes Demissie <Yohanesdemissie@gmail.com>
2019-08-19 18:33:42 -0700

    Updates

---
+++ precache-manifest.4b3a8eed6008e87220cbaa5cad7c0f0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "823ba4bfdaad5a73d34a4a63524cafb6",
+    "url": "/gh-deploy/index.html"
+  },
+  {
+    "revision": "7cd1ffb16182104fc13a",
+    "url": "/gh-deploy/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5892bb735162952d8467",
+    "url": "/gh-deploy/static/js/2.900ec463.chunk.js"
+  },
+  {
+    "revision": "7cd1ffb16182104fc13a",
+    "url": "/gh-deploy/static/js/main.9e4fa3cc.chunk.js"
+  },
+  {
+    "revision": "9a2d8c95361a0be89ec8",
+    "url": "/gh-deploy/static/js/runtime~main.88c09b95.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
