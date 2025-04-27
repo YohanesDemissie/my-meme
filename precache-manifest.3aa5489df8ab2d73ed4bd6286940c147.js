6f99d1aa3b374956d7b31ab973117a54dd2bb017
Yohanes Demissie <Yohanesdemissie@gmail.com>
2025-04-27 11:51:10 -0700

    Updates

---
+++ precache-manifest.3aa5489df8ab2d73ed4bd6286940c147.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aac55e82946978419a3c72274d50f54",
+    "url": "/my-meme/index.html"
+  },
+  {
+    "revision": "dc68d5ebf5a63f077aad",
+    "url": "/my-meme/static/css/main.f2501959.chunk.css"
+  },
+  {
+    "revision": "a2a67fea8f587eed050a",
+    "url": "/my-meme/static/js/2.90f49b3f.chunk.js"
+  },
+  {
+    "revision": "dc68d5ebf5a63f077aad",
+    "url": "/my-meme/static/js/main.07735dfb.chunk.js"
+  },
+  {
+    "revision": "89f82e6881de80e7c841",
+    "url": "/my-meme/static/js/runtime~main.04fb126e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-meme/precache-manifest.8f509ec4e716430ff456f7ccb7f8f363.js"
+  "/my-meme/precache-manifest.3aa5489df8ab2d73ed4bd6286940c147.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-meme/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
